ac1a9e43fc01a07e5b5dfc3283e873d99577e4f2
Ravi Putta <sp_ravikiran@yahoo.com>
2021-10-24 12:24:15 +0100

    DB,Model

---
+++ ETickets/Models/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Releationships
+
+        public List<Movie> Moives{ get; set; }
+    }
+}
